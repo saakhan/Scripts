e852e119e3eadb4865805d176c5d197e9803b700
parag <pnewalkar@gmail.com>
2016-07-05 12:49:55 +0100

    Ngnix installtion on Port 80

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -20,3 +20,14 @@ cookbook_file "/usr/share/nginx/www/index.html" do
   source "index.html"
   mode "0644"
 end
+
+# open port 80
+bash "allowing nginx traffic through firewall" do
+user "root"
+  code "ufw allow 80 && ufw allow 443"
+end
+
+execute "restart-nginx" do
+  command "/etc/init.d/nginx restart"
+  action :nothing
+end
